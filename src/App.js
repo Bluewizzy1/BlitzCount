1e9a5148a398e0ca86a098321f6683527415ea1b
Bluewizzy1 <125451191+Bluewizzy1@users.noreply.github.com>
2024-01-01 13:24:31 +0100

    create login page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import "./index.css";
 
 import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Topbar from "./components/Topbar";
 
 function App() {
   return (
-    <div className="bg-[#D2D2D2]">
+    <div className="bg-[#D2D2D2] w-full max-w-[fixed-width] px-[30px]">
       <Topbar/>
       <Routes>
         <Route>
           <Route index element={<Dashboard />} />
           <Route path="register" element={<SignUp />} />
+          <Route path="login" element={<Login />} />
         </Route>
       </Routes>
     </div>
